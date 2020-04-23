101c98fc35c3c7927d24b97e66389d0311ab8f43
StefanUnterholzner <02untste@rgtfo-me.it>
2020-04-23 17:14:09 +0200

    Methode zum erstellen eines Titels hinzugefügt

--- 02untste_Minirechner/02untste_Minirechner/Program.cs
+++ 02untste_Minirechner/02untste_Minirechner/Program.cs
@@ -9,6 +9,40 @@ namespace _02untste_Minirechner
     {
         static void Main(string[] args)
         {
+            Titelerstellen("Minirechner","", 0);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Erstellt an einer belibigen Stelle eine Überschrift
+        /// </summary>
+        /// <param name="Titel">Titel</param>
+        /// <param name="Untertitel">Untertittel, wenn "" wird der Untertitel weggelassen</param>
+        /// <param name="PositionTop">Possition des Beginn das Titels(Zeilen von Oben)</param>
+        static void Titelerstellen(String Titel, String Untertitel, int PositionTop)
+        {
+            Console.SetCursorPosition(0, PositionTop);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("-");
+            Console.SetCursorPosition((Console.WindowWidth-Titel.Length)/2,PositionTop+1);
+            Console.Write(Titel);
+            Console.SetCursorPosition(Console.WindowWidth-1, PositionTop + 1);
+            Console.Write("-");
+            if (Untertitel != "")
+            {
+                Console.Write("-");
+                Console.SetCursorPosition((Console.WindowWidth - Untertitel.Length)/2, PositionTop + 2);
+                Console.Write(Untertitel);
+                Console.SetCursorPosition(Console.WindowWidth - 1, PositionTop + 2);
+                Console.Write("-");
+            }
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("-");
+            }
+            
         }
     }
 }
